3881d1b9ad05b94e1f30b344eece6d65074f7965
github username <jtwerenski@gmail.com>
2019-08-04 11:35:33 -0500

    write out create posts migration with datatypes and configure the time zone in the application environment

--- db/migrate/20190803161952_create_posts.rb
+++ db/migrate/20190803161952_create_posts.rb
@@ -1,7 +1,10 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-
+      t.string :title
+      t.string :content
+      t.integer :user_id
+      t.datetime :date
       t.timestamps
     end
   end
