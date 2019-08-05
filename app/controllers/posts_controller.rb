8d9a184f844eacd2b3411890bfd85bca37d772ea
github username <jtwerenski@gmail.com>
2019-08-05 13:29:36 -0500

    detail posts index, create, show, update, and destroy actions as well as define strong params for posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,34 @@
 class PostsController < ApplicationController
-end 
+
+  def index
+    @posts = Post.by_date
+    render json: @posts
+  end
+
+  def create
+    @post = Post.new(post_params)
+    render json: @post if @post.save
+  end
+
+  def show
+    @post = Post.find_by(id: params[:id])
+    render json: @post
+  end
+
+  def update
+    @post = Post.find_by(id: params[:id])
+    render json: @post if @post.update
+  end
+
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content, :date, :user_id)
+  end
+
+end
