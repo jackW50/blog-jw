0d50aebdbd4685735c667313758752a12b1c3663
github username <jtwerenski@gmail.com>
2019-08-05 13:53:06 -0500

    add class method that returns all the posts ordered by date in descending order

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ApplicationRecord
   has_many :comments
   belongs_to :user
   has_many :commented_users, through: :comments, source: :user
+
+  def self.by_date
+    Post.order(date: :desc)
+  end
 end
