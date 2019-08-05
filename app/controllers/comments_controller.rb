3cc17db83716d704b6f9a4faceccd8387a704ef3
github username <jtwerenski@gmail.com>
2019-08-05 13:34:06 -0500

    detail create and destroy actions and the strong params in the comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,19 @@
 class CommnetsController < ApplicationController
-end 
+
+  def create
+    @comment = Comment.new(comment_params)
+
+    render json: @comment if @comment.save
+  end
+
+  def destroy
+    comment = Comment.find_by(id: params[:id])
+
+    comment.destroy
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text, :post_id, :user_id)
+end
