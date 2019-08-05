fc6b513ea90b6a34a7f3fd8681bab785158dcd77
github username <jtwerenski@gmail.com>
2019-08-05 13:19:54 -0500

    detail user create action and user_params in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
-end 
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: @user
+    end 
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username)
+  end
+end
